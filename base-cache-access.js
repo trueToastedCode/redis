8b1bdfbc3fa06b3bb17a418732f002a912d265bf
trueToastedCode <lennard2hp@gmail.com>
2023-04-20 13:04:07 +0200

    fix makeCache

--- base-cache-access.js
+++ base-cache-access.js
@@ -2,8 +2,9 @@ export default function makeBuildMakeCache ({ createClient }) {
   return function buildMakeCache ({ url }) {
     const client = createClient({ url })
     const connectionPromise = client.connect()
-    return function makeCache () {
-      return connectionPromise
+    return async function makeCache () {
+      await connectionPromise
+      return client
     }
   }
 }
